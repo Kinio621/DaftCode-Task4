22622e94201322aa39dc5b264020b181eeed2ba3
Kinio621 <44881531+Kinio621@users.noreply.github.com>
2020-04-06 17:02:03 +0200

    Add files via upload

---
+++ Task4.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, HTTPException
+app = FastAPI()
+
+from pydantic import BaseModel
+
+class Patient(BaseModel):
+    name: str
+    surename: str
+
+class PatientResp(BaseModel):
+    id: int
+    patient: Patient
+
+global i
+i = -1
+
+patients = []
+
+def increment():
+    global i
+    i = i + 1
+
+@app.post('/patient', response_model=NumerizedPatient)
+def post_patient(p: Patient):
+    global i
+    increment()
+    patients.append(p)
+    return NumerizedPatient(id=i, patient = p)
+
+@app.get('/patient/{pk}', response_model=Patient)
+def send_patient(pk: int):
+    global i
+    global patients
+    if (i < pk):
+        raise HTTPException(status_code=204, detail="Item not found")
+    else:
+        return patients[pk]
